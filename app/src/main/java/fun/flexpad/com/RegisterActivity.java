a7f232de1487f55751713e9e51bf2d1ef854ae10
MoteleJesu Olamilekan <60045247+Tele582@users.noreply.github.com>
2020-08-11 23:14:49 +0100

    The Everything App!
     Conflicts:
    	.gitignore

--- app/src/main/java/fun/flexpad/com/RegisterActivity.java
+++ app/src/main/java/fun/flexpad/com/RegisterActivity.java
@@ -4,8 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +24,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Objects;
 
@@ -59,6 +63,9 @@ public class RegisterActivity extends AppCompatActivity {
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                //sendEmail();
+
                 String txt_username = Objects.requireNonNull(username.getText()).toString();
                 String txt_email = Objects.requireNonNull(email.getText()).toString();
                 String txt_password = Objects.requireNonNull(password.getText()).toString();
@@ -67,10 +74,13 @@ public class RegisterActivity extends AppCompatActivity {
                 if (TextUtils.isEmpty(txt_username) || TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password) || TextUtils.isEmpty(contact_no)){
                     Toast.makeText(RegisterActivity.this, "All fields are required", Toast.LENGTH_SHORT).show();
                 } else if (txt_password.length() < 6) {
-                    Toast.makeText(RegisterActivity.this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RegisterActivity.this, "Password must have at least 6 characters", Toast.LENGTH_SHORT).show();
                 } else {
                     register(txt_username, txt_email, txt_password, contact_no);
+                    getEmailAddresses();
                 }
+
+
             }
         });
     }
@@ -101,9 +111,16 @@ public class RegisterActivity extends AppCompatActivity {
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task) {
                                     if (task.isSuccessful()){
+
                                         Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
                                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                                         startActivity(intent);
+
+                                        //getPhoneNumbers();
+                                        //getEmailAddresses();
+
+                                        //sendEmail();
+
                                         finish();
                                     }
                                 }
@@ -115,4 +132,97 @@ public class RegisterActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    public void getPhoneNumbers() {
+        String PHONE_NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, new String[]{PHONE_NUMBER}, null, null, null);
+        ArrayList<String> phones = new ArrayList<>();
+        assert cur != null;
+        while (cur.moveToNext()) {
+            String number = cur.getString(0);
+            number = number.replaceAll(" ", "");
+            if (!phones.contains(number)) phones.add(number);
+        }
+        cur.close();
+    }
+
+    public void getEmailAddresses() {
+        String EMAIL_ADDRESS = ContactsContract.CommonDataKinds.Email.ADDRESS;
+        ContentResolver car = getContentResolver();
+        Cursor cur = car.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, new String[]{EMAIL_ADDRESS}, null, null, null);
+        ArrayList<String> emails = new ArrayList<>();
+
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+
+        String txt_username = Objects.requireNonNull(username.getText()).toString();
+        String txt_email = Objects.requireNonNull(email.getText()).toString();
+
+        final String appPackageName = getApplicationContext().getPackageName();
+        String strAppLink = "";
+
+        try
+        {
+            strAppLink = "https://play.google.com/store/apps/details?id=" + appPackageName;
+        }
+        catch (android.content.ActivityNotFoundException anfe)
+        {
+            strAppLink = "https://play.google.com/store/apps/details?id=" + appPackageName;
+        }
+
+        String mEmail = ("telelekan@gmail.com");
+        String mSubject = ("Flexpad Alerts (Hi)");
+        String mMessage = ("Your contact, " + txt_email + " just signed up on Flexpad as '" + txt_username + "'. To sign up too, click here, " + strAppLink);
+
+
+        assert cur != null;
+        while (cur.moveToNext()) {
+            String number = cur.getString(0);
+            number = number.replaceAll(" ", "");
+            if (!emails.contains(number)) emails.add(number);
+
+            /*for (String mail_address : emails) {
+
+                JavaMailAPI javaMailAPI = new JavaMailAPI(this, mail_address, mSubject, mMessage); //from mEmail
+
+                javaMailAPI.execute();
+
+            }*/
+        }
+        cur.close();
+    }
+
+    private void sendEmail() {
+
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+
+        String txt_username = Objects.requireNonNull(username.getText()).toString();
+        String txt_email = Objects.requireNonNull(email.getText()).toString();
+
+        final String appPackageName = getApplicationContext().getPackageName();
+        String strAppLink = "";
+
+        try
+        {
+            strAppLink = "https://play.google.com/store/apps/details?id=" + appPackageName;
+        }
+        catch (android.content.ActivityNotFoundException anfe)
+        {
+            strAppLink = "https://play.google.com/store/apps/details?id=" + appPackageName;
+        }
+
+        String mEmail = ("telelekan@gmail.com");
+        String mSubject = ("Flexpad Alerts (Hi)");
+        String mMessage = ("Your contact, " + txt_email + " just signed up on Flexpad as '" + txt_username + "'. To sign up too, click here, " + strAppLink);
+
+        JavaMailAPI javaMailAPI = new JavaMailAPI(this, mEmail, mSubject, mMessage); //from mEmail
+
+        javaMailAPI.execute();
+
+    }
+
 }
+
+
