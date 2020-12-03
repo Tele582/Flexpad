bb3028e8289ad5205b8024d574bb4c5196825205
MoteleJesu Olamilekan <60045247+Tele582@users.noreply.github.com>
2020-12-03 17:30:05 +0100

    publishing (1) point

--- app/src/main/java/fun/flexpad/com/MessageActivity.java
+++ app/src/main/java/fun/flexpad/com/MessageActivity.java
@@ -653,7 +653,7 @@ public class MessageActivity extends AppCompatActivity {
                                 public void onResponse(Call<MyResponse> call, Response<MyResponse> response) {
                                     if (response.code() == 200){
                                         if (response.body().success != 1){
-                                            Toast.makeText(MessageActivity.this, "Failed!", Toast.LENGTH_SHORT).show();
+                                            Toast.makeText(MessageActivity.this, "Notification Failed!", Toast.LENGTH_SHORT).show();
                                         }
                                     }
                                 }
