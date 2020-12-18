9c00a1049167f8e6c294d4a02eb6a8a36c0afae5
MoteleJesu Olamilekan <60045247+Tele582@users.noreply.github.com>
2020-12-18 23:29:56 +0100

    show unseen voice_message numbers fixed

--- app/src/main/java/fun/flexpad/com/Fragments/RoomsFragment.java
+++ app/src/main/java/fun/flexpad/com/Fragments/RoomsFragment.java
@@ -132,7 +132,6 @@ public class RoomsFragment extends Fragment {
 
                                             }
                                         });
-
                                     }
 
                                     @Override
